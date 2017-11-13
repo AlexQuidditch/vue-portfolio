2b421fa4a1431317d46c4c97e629c98ccd40bdde
AlexQuidditch <alexquidditch@gmail.com>
2017-11-14 01:48:55 +0300

    start layout working

---
+++ src/components/layout/header/index.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <header class="header">
+    <div class="container">
+      <div class="header-container">
+        <h3 class="header__title">Сергей Иванов</h3>
+        <h4 class="header__sub-title">WEB / APP / UI DESIGN</h4>
+      </div>
+      <div class="header-container">
+        Меню
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Page-Header'
+  };
+</script>
+
+<style lang="scss">
+
+  .header {
+    position: absolute;
+    top: 0; left: 0;
+    width: 100%;
+
+  }
+
+</style>
