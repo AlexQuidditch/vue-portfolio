46f4f967435bae271668b81339b39fa6f8281a6c
AZabaykalsky <AlexQuidditch@yandex.ru>
2017-12-28 21:45:57 +0300

    fix

--- src/components/views/contact/index.vue
+++ src/components/views/contact/index.vue
@@ -136,142 +136,141 @@
       color: #ff144b;
       color: var(--neon-red);
     }
-  }
-
-  .contact-circle {
-    position: absolute;
-    top: 140px; left: 50%;
-    width: 728px;
-    height: 700px;
-    background-image: linear-gradient(125deg, #7bcbfb, #1c9bf7);
-    background-image: linear-gradient(125deg, #7bcbfb, var(--azure));
-    transform: translateX( -50% );
-    border-radius: 50%
-  }
-
-  .contact-form {
-    position: relative;
-    width: 100%;
-    margin-top: 115px;
-    padding: 60px 80px;
-    background-color: #fff;
-    background-color: var(--whited);
-    box-shadow: 5px 5px 20px 0 rgba(0, 0, 0, 0.3);
-    @include MQ(Pp) {
-      padding: 20px;
-      margin-top: 30px;
+    .contact-circle {
+      position: absolute;
+      top: 140px; left: 50%;
+      width: 728px;
+      height: 700px;
+      background-image: linear-gradient(125deg, #7bcbfb, #1c9bf7);
+      background-image: linear-gradient(125deg, #7bcbfb, var(--azure));
+      transform: translateX( -50% );
+      border-radius: 50%
     }
-    &__title {
-      font-family: 'Panton-Bold', Arial, Helvetica, sans-serif;
-      font-size: 36px;
-      font-weight: 800;
-      line-height: 1;
-      text-align: center;
-      color: #404552;
-      @include MQ(Pp) {
-        font-size: 8vw;
-      }
-    }
-    &__label {
+
+    .contact-form {
       position: relative;
-      display: block;
-      margin-top: 2.25rem;
-      margin-bottom: 2.25rem;
-      @include MQ(Pp) {
-        margin-top: 1.25rem;
-        margin-bottom: 1.25rem;
-      }
-    }
-    &__input {
-      display: block;
       width: 100%;
-      // height: 1.9rem;
-      // padding: 0.125rem 0.125rem 0.0625rem;
-      font-size: 1rem;
-      line-height: 1.9;
-      color: transparent;
-      background: none;
-      border-width: 0;
-      transition: all 0.28s ease;
-      border-color: transparent;
-      box-shadow: none;
-      resize: none;
+      margin-top: 115px;
+      padding: 60px 80px;
+      background-color: #fff;
+      background-color: var(--whited);
+      box-shadow: 5px 5px 20px 0 rgba(0, 0, 0, 0.3);
       @include MQ(Pp) {
-        font-size: 0.8rem;
+        padding: 20px;
+        margin-top: 30px;
       }
-      &::placeholder {
-        opacity: 0;
+      &__title {
+        font-family: 'Panton-Bold', Arial, Helvetica, sans-serif;
+        font-size: 36px;
+        font-weight: 800;
+        line-height: 1;
+        text-align: center;
+        color: #404552;
+        @include MQ(Pp) {
+          font-size: 8vw;
+        }
       }
-      &:focus,
-      &:valid {
-        color: #333;
-        & ~ .contact-form__placeholder {
+      &__label {
+        position: relative;
+        display: block;
+        margin-top: 2.25rem;
+        margin-bottom: 2.25rem;
+        @include MQ(Pp) {
+          margin-top: 1.25rem;
+          margin-bottom: 1.25rem;
+        }
+      }
+      &__input {
+        display: block;
+        width: 100%;
+        // height: 1.9rem;
+        // padding: 0.125rem 0.125rem 0.0625rem;
+        font-size: 1rem;
+        line-height: 1.9;
+        color: transparent;
+        background: none;
+        border-width: 0;
+        transition: all 0.28s ease;
+        border-color: transparent;
+        box-shadow: none;
+        resize: none;
+        @include MQ(Pp) {
           font-size: 0.8rem;
-          color: gray;
-          top: -1rem;
-          left: 0;
-          @include MQ(Pp) {
-            font-size: 0.75rem;
+        }
+        &::placeholder {
+          opacity: 0;
+        }
+        &:focus,
+        &:valid {
+          color: #333;
+          & ~ .contact-form__placeholder {
+            font-size: 0.8rem;
+            color: gray;
+            top: -1rem;
+            left: 0;
+            @include MQ(Pp) {
+              font-size: 0.75rem;
+            }
+          }
+        }
+        &:focus {
+          outline: none;
+          & ~ .contact-form__placeholder {
+            color: #1c9bf7;
+          }
+          & ~ .contact-form__bar::before {
+            width: 100%;
+            left: 0;
           }
         }
       }
-      &:focus {
-        outline: none;
-        & ~ .contact-form__placeholder {
-          color: #1c9bf7;
-        }
-        & ~ .contact-form__bar::before {
-          width: 100%;
-          left: 0;
-        }
-      }
-    }
-    &__placeholder {
-      position: absolute;
-      display: block;
-      top: 0.25rem;
-      pointer-events: none;
-      padding-left: 0.125rem;
-      z-index: 1;
-      color: #b3b3b3;
-      font-size: 1rem;
-      font-weight: normal;
-      transition: all 0.28s ease;
-      @include MQ(Pp) {
-        font-size: 0.9rem;
-      }
-    }
-    &__bar {
-      position: relative;
-      display: block;
-      border-bottom: 0.0625rem solid #999;
-      &::before {
-        content: '';
-        height: 0.125rem;
-        width: 0;
-        left: 50%;
-        bottom: -0.0625rem;
+      &__placeholder {
         position: absolute;
-        background-color: #1c9bf7;
-        transition: left 0.28s ease, width 0.28s ease;
-        z-index: 2;
+        display: block;
+        top: 0.25rem;
+        pointer-events: none;
+        padding-left: 0.125rem;
+        z-index: 1;
+        color: #b3b3b3;
+        font-size: 1rem;
+        font-weight: normal;
+        transition: all 0.28s ease;
+        @include MQ(Pp) {
+          font-size: 0.9rem;
+        }
       }
-    }
-    &__submit {
-      width: 200px;
-      height: 50px;
-      margin-top: 30px;
-      font-family: 'Panton-Bold', Arial, Helvetica, sans-serif;
-      font-size: 18px;
-      text-align: center;
-      color: #fff;
-      border-radius: 100px;
-      background-color: #ff144b;
-      background-color: var(--neon-red);
-      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
-      @include MQ(Pp) {
-        width: 100%;
-        margin-top: 0;
+      &__bar {
+        position: relative;
+        display: block;
+        border-bottom: 0.0625rem solid #999;
+        &::before {
+          content: '';
+          height: 0.125rem;
+          width: 0;
+          left: 50%;
+          bottom: -0.0625rem;
+          position: absolute;
+          background-color: #1c9bf7;
+          transition: left 0.28s ease, width 0.28s ease;
+          z-index: 2;
+        }
+      }
+      &__submit {
+        width: 200px;
+        height: 50px;
+        margin-top: 30px;
+        font-family: 'Panton-Bold', Arial, Helvetica, sans-serif;
+        font-size: 18px;
+        text-align: center;
+        color: #fff;
+        border-radius: 100px;
+        background-color: #ff144b;
+        background-color: var(--neon-red);
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
+        @include MQ(Pp) {
+          width: 100%;
+          margin-top: 0;
+        }
       }
     }
   }
