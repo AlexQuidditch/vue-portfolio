582e299455335187d47928ec84641afe7383c502
AZabaykalsky <AlexQuidditch@yandex.ru>
2018-01-15 22:53:11 +0300

    added meta information

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Contact from '../components/views/contact/index.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: __dirname,
   linkExactActiveClass: '_exact-active',
@@ -20,32 +20,86 @@ export default new Router({
       path: '/:Case',
       name: 'Base',
       component: Base,
-      props: true
+      props: true,
+      meta: {
+        title: 'Сергей Иванов WEB | / APP / UI DESIGN',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Сергей Иванов WEB | WEB / APP / UI DESIGN'
+          }
+        ]
+      }
     },
     {
       path: '/case/v-dele',
       name: 'VDele',
-      component: VDele
+      component: VDele,
+      meta: {
+        title: 'Кейс - Время Пришло',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Разработка интерфейса студенческой фриланс - биржи "Время Пришло"'
+          }
+        ]
+      }
     },
     {
       path: '/case/travel-app',
       name: 'TravelApp',
-      component: TravelApp
+      component: TravelApp,
+      meta: {
+        title: 'Кейс - Travel.App',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Веб-сервис и приложение геолокации, отличительной особенностью которого являются 3D - карты реального времени.'
+          }
+        ]
+      }
     },
     {
       path: '/case/mpu',
       name: 'MPU',
-      component: MPU
+      component: MPU,
+      meta: {
+        title: 'Кейс - MPU Leicht',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Разработка дизайна сайта и веб - приложения MPU Leicht'
+          }
+        ]
+      }
     },
     {
       path: '/about/me',
       name: 'About',
-      component: About
+      component: About,
+      meta: {
+        title: 'Обо мне || Портфолио',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Привет! Я занимаюсь дизайном сайтов, веб-сервисов и приложений, делая их привлекательнее для Ваших клиентов. Популизирую положительный имидж компании, увеличиваю прибыль.'
+          }
+        ]
+      }
     },
     {
       path: '/contact/me',
       name: 'Contact',
-      component: Contact
+      component: Contact,
+      meta: {
+        title: 'Напишите мне || Портфолио',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Напишите мне и мы детально обсудим Ваше предложение'
+          }
+        ]
+      }
     },
     {
       path: '*',
@@ -53,3 +107,31 @@ export default new Router({
     }
   ]
 });
+
+// This callback runs before every route change, including on page load.
+router.beforeEach( ( to , from , next ) => {
+  const nearestWithTitle = to.matched.slice().reverse().find( r => r.meta && r.meta.title );
+  const nearestWithMeta = to.matched.slice().reverse().find( r => r.meta && r.meta.metaTags );
+  const previousNearestWithMeta = from.matched.slice().reverse().find( r => r.meta && r.meta.metaTags );
+
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  Array.from( document.querySelectorAll('[data-vue-router-controlled]')).map( el => el.parentNode.removeChild(el) );
+
+  if (!nearestWithMeta) return next();
+
+  nearestWithMeta.meta.metaTags.map( tagDef => {
+    const tag = document.createElement('meta');
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+    tag.setAttribute('data-vue-router-controlled', '');
+    return tag;
+  })
+    // Add the meta tags to the document head.
+    .forEach( tag => document.head.appendChild(tag) );
+
+  next();
+});
+
+export default router;
